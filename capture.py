24c81f08c4f8dc5d5ade14dfebc1578ec107a186
blaz-r <blaz.rolih@gmail.com>
2022-06-30 16:15:01 +0200

    Added function comments and adjusted to capture 20 videos

--- capture.py
+++ capture.py
@@ -1,52 +1,7 @@
 import time
-from math import gcd
-
 import cv2
-import mediapipe as mp
 import depthai as dai
-
-def find_isp_scale_params(size, resolution, is_height=True):
-    """
-    Find closest valid size close to 'size' and and the corresponding parameters to setIspScale()
-    This function is useful to work around a bug in depthai where ImageManip is scrambling images that have an invalid size
-    resolution: sensor resolution (width, height)
-    is_height : boolean that indicates if the value 'size' represents the height or the width of the image
-    Returns: valid size, (numerator, denominator)
-    """
-    # We want size >= 288 (first compatible size > lm_input_size)
-    if size < 288:
-        size = 288
-
-    width, height = resolution
-
-    # We are looking for the list on integers that are divisible by 16 and
-    # that can be written like n/d where n <= 16 and d <= 63
-    if is_height:
-        reference = height
-        other = width
-    else:
-        reference = width
-        other = height
-    size_candidates = {}
-    for s in range(288, reference, 16):
-        f = gcd(reference, s)
-        n = s // f
-        d = reference // f
-        if n <= 16 and d <= 63 and int(round(other * n / d) % 2 == 0):
-            size_candidates[s] = (n, d)
-
-    # What is the candidate size closer to 'size' ?
-    min_dist = -1
-    for s in size_candidates:
-        dist = abs(size - s)
-        if min_dist == -1:
-            min_dist = dist
-            candidate = s
-        else:
-            if dist > min_dist: break
-            candidate = s
-            min_dist = dist
-    return candidate, size_candidates[candidate]
+from dai_utils import create_pipeline, find_isp_scale_params
 
 
 resolution = (1920, 1080)
@@ -64,34 +19,19 @@ pad_h = (img_w - img_h) // 2
 frame_size = img_w
 
 
-def create_pipeline():
-    pipeline = dai.Pipeline()
-
-    print("Adding RGB camera")
-    rgb_cam = pipeline.createColorCamera()
-    rgb_cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-    rgb_cam.setBoardSocket(dai.CameraBoardSocket.RGB)
-    rgb_cam.setInterleaved(False)
-    rgb_cam.setFps(30)
-    rgb_cam.setIspScale(scale_nd[0], scale_nd[1])
-    rgb_cam.setVideoSize(img_w, img_h)
-    rgb_cam.setPreviewSize(img_w, img_h)
-
-    print("Adding video out to queue")
-    cam_out = pipeline.createXLinkOut()
-    cam_out.setStreamName("cam_out")
-    cam_out.input.setQueueSize(1)
-    cam_out.input.setBlocking(False)
-    rgb_cam.video.link(cam_out.input)
-
-    return pipeline
-
-
 def view():
+    """
+    View video directly from camera
+
+    :return: None
+    """
+
+    # create device with pipeline, with same params as final project
     device = dai.Device()
     device.startPipeline(create_pipeline())
     img_q = device.getOutputQueue(name="cam_out", maxSize=1, blocking=False)
 
+    # capture and display video
     while True:
         image = img_q.get().getCvFrame()
         cv2.imshow("View", image)
@@ -100,21 +40,29 @@ def view():
             return
 
 
-
 def capture(path, name):
+    """
+    Capture gesture videos.
+    Each call captures 20 videos with 30 frames each.
+    All are 30fps with 1152 x 648 resolution
+
+    :param path: path to gesture directory
+    :param name: filename of video
+    :return: None
+    """
     device = dai.Device()
     device.startPipeline(create_pipeline())
     img_q = device.getOutputQueue(name="cam_out", maxSize=1, blocking=False)
 
     image = img_q.get().getCvFrame()
 
-    for cap in range(30):
+    for cap in range(20):
 
         last_time = time.time()
-        while time.time() - last_time < 3:
+        while time.time() - last_time < 2:
             image = img_q.get().getCvFrame()
 
-            cv2.putText(image, f"Starting collection in {int(4 - (time.time() - last_time))} seconds",
+            cv2.putText(image, f"Starting collection in {int(3 - (time.time() - last_time))} seconds",
                         (120, 200), cv2.FONT_HERSHEY_SIMPLEX,
                         0.5, (0, 120, 255), 4, cv2.LINE_AA)
             cv2.imshow("Capture", image)
@@ -144,8 +92,14 @@ def capture(path, name):
     cv2.destroyAllWindows()
 
 
-def read_test():
-    cap = cv2.VideoCapture("G:\Vids\Davinci\geste_test\CutTest.mp4")
+def read_test(path):
+    """
+    Show video from file
+
+    :param path: path to video
+    :return: None
+    """
+    cap = cv2.VideoCapture(path)
 
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
@@ -158,8 +112,8 @@ def read_test():
     while cap.isOpened():
         ret, frame = cap.read()
 
-        progress += 1
         print(f"Progress: {progress}/{frame_count}")
+        progress += 1
 
         if ret:
             cv2.imshow("Read", frame)
@@ -171,6 +125,6 @@ def read_test():
 
 
 if __name__ == "__main__":
-    # view()
-    capture("gesture_videos/play", "play_capture")
-    # read_test()
+    view()
+    capture("gesture_videos/pause", "pause_capture")
+    # read_test("gesture_videos/play/play_capture1.mp4")
