c89c09f1bd6fbe86a163a1f5b190657c480ad3f0
blaz-r <blaz.rolih@gmail.com>
2022-07-03 19:03:53 +0200

    Separated live test code and testing scaling

--- train_model.py
+++ train_model.py
@@ -1,7 +1,10 @@
+import onnxruntime
 import tensorflow.python.keras as keras
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Input
+from tensorflow.python.tools import freeze_graph
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -146,7 +149,7 @@ def prepare_data(path):
         result = gestures_onehot_dict[gesture]
         for file in os.listdir(gesture_path):
             file_path = f"{gesture_path}/{file}"
-            landmarks = np.load(file_path)
+            landmarks = np.load(file_path) / 10000
 
             sequences.append(landmarks)
             results.append(result)
@@ -188,7 +191,7 @@ def train_lstm():
 
     model.compile(optimizer="Adam", loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
-    model.fit(X_train, y_train, epochs=600)
+    model.fit(X_train, y_train, epochs=200)
 
     model.summary()
 
@@ -226,7 +229,7 @@ def test_model():
 
 
 def test_on_landmarks():
-    data = np.load("G:\Faks\diploma\gesture_capture\gesture_landmarks\play\play_1.npy")
+    data = np.load("G:\Faks\diploma\gesture_capture\gesture_landmarks\play\play_1.npy") / 10000
     data = np.expand_dims(data, axis=0)
 
     model = make_model()
@@ -239,129 +242,20 @@ def test_on_landmarks():
     print(t1 - t0, res)
 
 
-def live_test():
-    """
-    Test gesture recognition live
-
-    :return:
-    """
-    # load model architecture and weight
+def save_model():
     model = make_model()
     model.load_weights("gestures.h5")
 
-    gestures = ["play", "pause", "idle"]
-
-    device = dai.Device()
-    device.startPipeline(create_pipeline())
-    img_q = device.getOutputQueue(name="cam_out", maxSize=1, blocking=False)
-
-    with mp_hands.Hands(
-            max_num_hands=1,
-            model_complexity=0,
-            min_detection_confidence=0.2,
-            min_tracking_confidence=0.5) as hands:
-
-        sequence = []
-        predictions = []
-        threshold = 0.5
-        sentence = []
-
-        while True:
-
-            image = img_q.get().getCvFrame()
-
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            results = hands.process(image)
-
-            # Draw the hand annotations on the image.
-            image.flags.writeable = True
-
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        image,
-                        hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style())
-
-                # extract and sacale landmarks
-                landmarks = extract_landmarks(results)
-                sequence.append(landmarks)
-                # keep last 30 frames
-                sequence = sequence[-30:]
-
-            if len(sequence) == 30:
-                # predict with lstm
-                result = model.predict(np.expand_dims(sequence, axis=0))[0]
-                predictions.append(np.argmax(result))
-                print(result)
-
-                # output if last 20 frames are all same prediction
-                if np.unique(predictions[-20:])[0] == np.argmax(result):
-                    if result[np.argmax(result)] > threshold:
-
-                        if len(sentence) > 0:
-                            if gestures[np.argmax(result)] != sentence[-1]:
-                                sentence.append(gestures[np.argmax(result)])
-                        else:
-                            sentence.append(gestures[np.argmax(result)])
-
-                if len(sentence) > 5:
-                    sentence = sentence[-5:]
-
-            cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
-            cv2.putText(image, ' '.join(sentence), (3, 30),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-            cv2.imshow('MediaPipe Hands', image)
-            key = cv2.waitKey(5)
-            if key == 27 or key == ord('q'):
-                break
+    MODEL_DIR = "gesture_recognition_lstm"
+    tf.saved_model.save(model, MODEL_DIR)
 
 
-def live_mediapipe():
-    """
-    Mediapipe hand detection from live camera
+def check_weights():
+    model = make_model()
+    model.load_weights("gestures.h5")
 
-    :return: None
-    """
-    device = dai.Device()
-    device.startPipeline(create_pipeline())
-    img_q = device.getOutputQueue(name="cam_out", maxSize=1, blocking=False)
-
-    with mp_hands.Hands(
-            max_num_hands=1,
-            model_complexity=0,
-            min_detection_confidence=0.2,
-            min_tracking_confidence=0.5) as hands:
-        while True:
-
-            image = img_q.get().getCvFrame()
-
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            results = hands.process(image)
-
-            # Draw the hand annotations on the image.
-            image.flags.writeable = True
-
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        image,
-                        hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style())
-
-            cv2.imshow('MediaPipe Hands', image)
-            key = cv2.waitKey(5)
-            if key == 27 or key == ord('q'):
-                break
+    weights = model.get_weights()
+    print(weights)
 
 
 def main():
@@ -378,6 +272,7 @@ if __name__ == '__main__':
     # main()
     # live_mediapipe()
     # train_lstm()
+    # save_model()
+    # check_weights()
     # test_model()
     test_on_landmarks()
-    # live_test()
